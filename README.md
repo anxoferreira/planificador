07eedd7135c4740004d72fd80d74de0eabe3c32f
anxoferreira <anxoferreira07@gmail.com>
2026-09-16 13:46:32 +0200

    subo esto

---
+++ README.md
@@ -0,0 +1,23 @@
+## inicializar el proyexto
+
+npm init
+
+## descargar dependencia de desarrollo (vite)
+
+npm i -D vite
+
+## descargar una dependencia de produccion en este caso dayjs
+
+npm i dayjs
+
+## probar la app en modo local (desarrollo)
+
+npm run dev
+
+## construir la app
+
+npm run build
+
+## arrancar la app en modo produccion
+
+npm run preview
---
+++ src/main.js
@@ -0,0 +1,21 @@
+//importar la libreria dayjs
+import dayjs from "dayjs"
+
+//boton referenciado por id
+const boton = document.getElementById("anadir")
+//cuadro del texto referenciado por id
+const input = document.getElementById("texto")
+//lista referenciada por id
+const lista = document.getElementById("lista")
+
+//poner el boton a la escucha del evento click y que ejecute la funcion guardarPlan
+boton.addEventListener("click", guardarPlan)
+
+//funcion que guarda el plan en localStorage
+function guardarPlan() {
+    const fechaHoy = dayjs().format("DD/MM/YYYY HH:mm")
+    const li = document.createElement("li")
+    li.textContent = input.value + " - " + fechaHoy
+    lista.appendChild(li)
+    input.value = ""
+}
